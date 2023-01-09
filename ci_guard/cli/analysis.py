6ee0d510d605091d16508727c3938194533c7114
jiangpengjuj <jiangpengju1@yeah.net>
2023-01-09 18:53:25 +0800

    obs 编译代码(处理 pr 拉取的代码的脚本, 引用旧门禁代码)

--- ci_guard/cli/analysis.py
+++ ci_guard/cli/analysis.py
@@ -10,3 +10,36 @@
 # PURPOSE.
 # See the Mulan PSL v2 for more details.
 # ******************************************************************************/
+import os.path
+import sys
+
+import click
+
+from conf import config
+from core.analysis import Analysis
+from exception import FileError
+
+
+@click.command("analysis", help="Analysis of package variation content and scope of impact")
+@click.option(
+    "-df",
+    "--diff-file",
+    default=config.compare_result,
+    show_default=True,
+    help="Output file of the package difference comparison tool comparison results",
+)
+def diff_analysis(diff_file):
+    click.echo("[INFO] Start compare package")
+    if not diff_file or not os.path.exists(diff_file):
+        raise FileError(
+            msg=f"Package comparison result file: {diff_file} does not exist, please confirm the file path"
+        )
+    analysis_obj = Analysis(diff_file=diff_file)
+    effect_detail = analysis_obj.depended_analysis()
+    if effect_detail.get("need_verify"):
+        click.echo(click.style(f"Need to continue to verify,{effect_detail}", fg="red"))
+    else:
+        click.echo(click.style(f"No verification needed,{effect_detail}", fg="green"))
+        sys.exit(1)
+
+__all__ = "diff_analysis"
