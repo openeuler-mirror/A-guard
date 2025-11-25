51aab8f141efc44a7e606dd13fd47e3aeaa8f1cd
xiaoniuzi <dingxudong1@huawei.com>
2025-11-25 10:10:42 +0800

    gitee切换到gitcode的适配

--- ci_guard/ci-guard.sh
+++ ci_guard/ci-guard.sh
@@ -7,9 +7,12 @@ SCRIPT_PATCH=${shell_pathoe}/src/build
 if [[ ${platform} == "github" ]]; then
     repo_server_test_tail="-github"
     pr=https://github.com/${repo_owner}/${repo}/pull/${prid}
-else
+elif [[ ${platform} == "gitee" ]]; then
     repo_server_test_tail=""
     pr=https://gitee.com/${repo_owner}/${repo}/pull/${prid}
+else
+    repo_server_test_tail=""
+    pr=https://gitcode.com/${repo_owner}/${repo}/pull/${prid}
 fi
 
 fileserver_user_path="/repo/openeuler/src-openeuler${repo_server_test_tail}/${tbranch}/${committer}/${repo}/${arch}/${prid}/${repo_comment}/$commentid"
@@ -46,7 +49,7 @@ EOF
 
 function update_config(){
     echo "============ Start synchronizing jenkin environment variables ============"
-    sed -i "/^gitee_token: */cgitee_token: ${GiteeToken}" ${shell_path}/ci_guard/conf/config.yaml
+    sed -i "/^gitcode_token: */cgitcode_token: ${gitcodeToken}" ${shell_path}/ci_guard/conf/config.yaml
     sed -i "/^requires_repo: */crequires_repo: ${buddy}" ${shell_path}/ci_guard/conf/config.yaml
     sed -i "/^build_env_account: */cbuild_env_account: ${OBSSecondaryUserName}" ${shell_path}/ci_guard/conf/config.yaml
     sed -i "/^build_env_passwd: */cbuild_env_passwd: ${OBSSecondaryPassword}" ${shell_path}/ci_guard/conf/config.yaml
@@ -170,11 +173,11 @@ function compare_difference(){
 }
 
 function abi_compare(){
-    pr_link='https://gitee.com/${repo_owner}/'${repo}'/pulls/'${prid}
+    pr_link='https://gitcode.com/${repo_owner}/'${repo}'/pulls/'${prid}
     pr_commit_json_file="${WORKSPACE}/pr_commit_json_file"
     # comment_file="${repo}_${prid}_${arch}_comment"
     if [[ ${platform} != "github" ]]; then
-        curl https://gitee.com/api/v5/repos/${repo_owner}/${repo}/pulls/${prid}/files?access_token=$GiteeToken >$pr_commit_json_file
+        curl https://api.gitcode.com/api/v5/repos/${repo_owner}/${repo}/pulls/${prid}/files?access_token=$gitcodeToken >$pr_commit_json_file
     fi
     compare_result="${repo}_${prid}_${arch}_compare_result"
     export PYTHONPATH=${shell_pathoe}
@@ -240,7 +243,7 @@ EOF
 
     python3 ${shell_pathoe}/src/utils/oemaker_analyse.py --branch ${tbranch} --arch ${arch} \
 	--oecp_json_path "$result_dir/report-$old_dir-$new_dir/osv.json" --owner "src-openeuler" \
-	--repo ${repo} --gitee_token $GiteeToken --prid ${prid}
+	--repo ${repo} --gitcode_token $gitcodeToken --prid ${prid}
 }
 
 function check_single_build(){
@@ -381,7 +384,7 @@ function print_job(){
     job_name=`echo $JOB_NAME|sed -e 's#/#/job/#g'`
     job_path="https://openeulerjenkins.osinfra.cn/job/${job_name}/$BUILD_ID/console"
     body_str="${arch}架构构建及构建后检查：<a href=${job_path}>${JOB_NAME}/${BUILD_ID}/console</a>"
-    curl -X POST --header 'Content-Type: application/json;charset=UTF-8' 'https://gitee.com/api/v5/repos/src-openeuler/'${repo}'/pulls/'${prid}'/comments' -d '{"access_token":"'"${GiteeToken}"'","body":"'"${body_str}"'"}' || echo "comment source pr failed"
+    curl -X POST --header 'Content-Type: application/json;charset=UTF-8' 'https://api.gitcode.com/api/v5/repos/src-openeuler/'${repo}'/pulls/'${prid}'/comments' -d '{"access_token":"'"${gitcodeToken}"'","body":"'"${body_str}"'"}' || echo "comment source pr failed"
 }
 
 function main(){
