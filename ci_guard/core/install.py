064be87afcd9fdf8b124205ac3cf02f52413a19e
xiaoniuzi <dingxudong1@huawei.com>
2025-11-27 10:41:15 +0800

    gitee切换到gitcode的适配:适配新的Jenkins工程结构

--- ci_guard/core/install.py
+++ ci_guard/core/install.py
@@ -458,7 +458,6 @@ class UnifyBuildInstallVerify(InstallBase):
     def _get_emsx(self, project):
         cmds = f"ccb select projects os_project={project}"
         code, cmd_out, error = command(cmds=cmds.split(), console=False)
-        logger.info("the project and output in _get_emsx are {} {}".format(project, cmd_out))
         if code:
             logger.error(
                 f"Failed to get the project info,command: {cmds} error: {error}"
