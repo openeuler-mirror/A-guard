c8a5ec7b8495b8c3328afc7316d4feb3d3d74dfe
goumenghao <goumenghao@h-partners.com>
2026-07-17 15:31:43 +0800

    CI support openEuler-26.09
    Signed-off-by: goumenghao <goumenghao@h-partners.com>

--- ci_guard/constant.py
+++ ci_guard/constant.py
@@ -73,6 +73,7 @@ OS_VARIANR_MAP = {"openEuler-22.03-LTS-SP1": "openEuler:22.03-LTS-Next",
                   "openEuler-25.09": "openEuler:25.09",
                   "openEuler-24.03-LTS-SP3": "openEuler:24.03-LTS-SP3",
                   "openEuler-24.03-LTS-SP4": "openEuler:24.03-LTS-SP4",
+                  "openEuler-26.09": "openEuler:26.09",
                   "master": "openEuler:mainline"}
 
 BOOTSTRAP_MAP = {
